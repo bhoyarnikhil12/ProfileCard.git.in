4f84a40de1cb618be3523988e8871bed3ba571c4
bhoyarnikhil12 <140892322+bhoyarnikhil12@users.noreply.github.com>
2024-12-13 08:10:15 +0530

    Create Profile.js

---
+++ src/api/Profile.js
@@ -0,0 +1,33 @@
+export const getProfiles = async () => {
+    return [
+      {
+        id: 1,
+        name: 'John Doe',
+        photo: 'https://static.vecteezy.com/system/resources/thumbnails/041/646/466/small/ai-generated-portrait-of-handsome-young-man-png.png',
+        description: 'Software Engineer',
+        address: { lat: 37.7749, lng: -122.4194 },
+      },
+      {
+        id: 2,
+        name: 'Jane Smith',
+        photo: 'https://static.vecteezy.com/system/resources/thumbnails/041/642/170/small/ai-generated-portrait-of-handsome-smiling-young-man-with-folded-arms-isolated-free-png.png',
+        description: 'Designer',
+        address: { lat: 34.0522, lng: -118.2437 },
+      },
+      {
+        id: 3,
+        name: 'Michal Clarke',
+        photo: 'https://static.vecteezy.com/system/resources/thumbnails/041/642/171/small/ai-generated-portrait-of-handsome-smiling-young-man-with-folded-arms-isolated-free-png.png',
+        description: 'Developer',
+        address: { lat: 34.0522, lng: -118.2437 },
+      },
+      {
+        id: 4,
+        name: 'Ricky Ponting',
+        photo: 'https://static.vecteezy.com/system/resources/thumbnails/041/642/159/small_2x/ai-generated-portrait-of-handsome-smiling-young-man-with-folded-arms-isolated-free-png.png',
+        description: 'Tester',
+        address: { lat: 34.0522, lng: -118.2437 },
+      },
+    ];
+  };
+  
